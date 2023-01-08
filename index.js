37bab6264f9ca57370c095f7baf4a76c31b9f547
Niklas Ling <72392907+nling43@users.noreply.github.com>
2023-01-08 01:07:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,19 +7,19 @@ const PORT = process.env.PORT || 3000;
 
 const carpenterOrders = [
 	{
-		OrderNumber: "2022-22-21-11",
+		OrderNumber: "2022-12-21-11",
 		expectHours: 5,
 	},
 	{
-		OrderNumber: "2022-22-22-12",
+		OrderNumber: "2022-12-22-12",
 		expectHours: 4,
 	},
 	{
-		OrderNumber: "2022-22-23-13",
+		OrderNumber: "2022-12-23-13",
 		expectHours: 9,
 	},
 	{
-		OrderNumber: "2022-22-24-14",
+		OrderNumber: "2022-12-24-14",
 		expectHours: 20,
 	},
 ];
@@ -43,25 +43,25 @@ const blacksmithOrders = [
 ];
 const deliveryOrders = [
 	{
-		OrderNumber: "2022-22-21-11",
-		expectHours: 1,
+		OrderNumber: "2022-32-21-11",
+		expectHours: 4,
 		address: "Glasbruksvagen 32",
 		contact: ["Ingvor Svensson", "070604502"],
 	},
 	{
-		OrderNumber: "2022-22-22-12",
-		expectHours: 2,
+		OrderNumber: "2022-32-22-12",
+		expectHours: 5,
 		address: "Gustav III:s vag 28",
 		contact: ["Viveka Lindberg", "070604502"],
 	},
 	{
-		OrderNumber: "2022-22-23-13",
+		OrderNumber: "2022-32-23-13",
 		expectHours: 3,
 		address: "Kilbacksgatan 36",
 		contact: ["Hildegard Nilsson", "070604502"],
 	},
 	{
-		OrderNumber: "2022-22-24-14",
+		OrderNumber: "2022-32-24-14",
 		expectHours: 4,
 		address: "Vargatan 2",
 		contact: ["Jacob Nordström", "070604502"],
@@ -72,6 +72,42 @@ const products = [
 		count: 10,
 		id: "1192173",
 		name: "Utbildningsbord Hopfällbart",
+		OrderNumber: "2022-32-21-11",
+	},
+	{
+		count: 5,
+		id: "1175536",
+		name: "Kontorsstol Plus 8 hög rygg",
+		OrderNumber: "2022-32-21-11",
+	},
+	{
+		count: 1,
+		id: "1190278",
+		name: "Öppen förvaring 4xA4",
+		OrderNumber: "2022-2-21-11",
+	},
+	{
+		count: 10,
+		id: "1192173",
+		name: "Utbildningsbord Hopfällbart",
+		OrderNumber: "2022-12-21-11",
+	},
+	{
+		count: 5,
+		id: "1175536",
+		name: "Kontorsstol Plus 8 hög rygg",
+		OrderNumber: "2022-12-21-11",
+	},
+	{
+		count: 1,
+		id: "1190278",
+		name: "Öppen förvaring 4xA4",
+		OrderNumber: "2022-12-21-11",
+	},
+	{
+		count: 10,
+		id: "1192173",
+		name: "Utbildningsbord Hopfällbart",
 		OrderNumber: "2022-22-21-11",
 	},
 	{
@@ -86,7 +122,24 @@ const products = [
 		name: "Öppen förvaring 4xA4",
 		OrderNumber: "2022-22-21-11",
 	},
-
+	{
+		count: 2,
+		id: "1192028",
+		name: "Soffbord runt",
+		OrderNumber: "2022-32-22-12",
+	},
+	{
+		count: 3,
+		id: "1192224",
+		name: "Whiteboard Mood Wall glas",
+		OrderNumber: "2022-32-22-12",
+	},
+	{
+		count: 1,
+		id: "1192337",
+		name: "Konferensbord Slim Table Plus",
+		OrderNumber: "2022-32-22-12",
+	},
 	{
 		count: 2,
 		id: "1192028",
@@ -108,6 +161,37 @@ const products = [
 
 	{
 		count: 2,
+		id: "1192028",
+		name: "Soffbord runt",
+		OrderNumber: "2022-12-22-12",
+	},
+	{
+		count: 3,
+		id: "1192224",
+		name: "Whiteboard Mood Wall glas",
+		OrderNumber: "2022-12-22-12",
+	},
+	{
+		count: 1,
+		id: "1192337",
+		name: "Konferensbord Slim Table Plus",
+		OrderNumber: "2022-12-22-12",
+	},
+	{
+		count: 2,
+		id: "1191634",
+		name: "Karmstol Öland",
+		OrderNumber: "2022-32-23-13",
+	},
+	,
+	{
+		count: 1,
+		id: "1191334",
+		name: "Matbord / Konferenssbord runt",
+		OrderNumber: "2022-32-23-13",
+	},
+	{
+		count: 2,
 		id: "1191634",
 		name: "Karmstol Öland",
 		OrderNumber: "2022-22-23-13",
@@ -119,7 +203,19 @@ const products = [
 		name: "Matbord / Konferenssbord runt",
 		OrderNumber: "2022-22-23-13",
 	},
-
+	{
+		count: 2,
+		id: "1191634",
+		name: "Karmstol Öland",
+		OrderNumber: "2022-12-23-13",
+	},
+	,
+	{
+		count: 1,
+		id: "1191334",
+		name: "Matbord / Konferenssbord runt",
+		OrderNumber: "2022-12-23-13",
+	},
 	{
 		count: 10,
 		id: "1192173",
@@ -170,6 +266,84 @@ const products = [
 		id: "1191334",
 		name: "Matbord / Konferenssbord runt",
 		OrderNumber: "2022-22-24-14",
+	},
+	,
+	{
+		count: 10,
+		id: "1192173",
+		name: "Utbildningsbord Hopfällbart",
+		OrderNumber: "2022-12-24-14",
+	},
+	{
+		count: 5,
+		id: "1175536",
+		name: "Kontorsstol Plus 8 hög rygg",
+		OrderNumber: "2022-12-24-14",
+	},
+	{
+		count: 1,
+		id: "1190278",
+		name: "Öppen förvaring 4xA4",
+		OrderNumber: "2022-12-24-14",
+	},
+
+	{
+		count: 2,
+		id: "1192028",
+		name: "Soffbord runt",
+		OrderNumber: "2022-12-24-14",
+	},
+	{
+		count: 3,
+		id: "1192224",
+		name: "Whiteboard Mood Wall glas",
+		OrderNumber: "2022-12-24-14",
+	},
+	{
+		count: 1,
+		id: "1192337",
+		name: "Konferensbord Slim Table Plus",
+		OrderNumber: "2022-12-24-14",
+	},
+
+	{
+		count: 2,
+		id: "1191634",
+		name: "Karmstol Öland",
+		OrderNumber: "2022-12-24-14",
+	},
+	,
+	{
+		count: 1,
+		id: "1191334",
+		name: "Matbord / Konferenssbord runt",
+		OrderNumber: "2022-12-24-14",
+	},
+	{
+		count: 3,
+		id: "1192224",
+		name: "Whiteboard Mood Wall glas",
+		OrderNumber: "2022-12-24-14",
+	},
+	{
+		count: 1,
+		id: "1192337",
+		name: "Konferensbord Slim Table Plus",
+		OrderNumber: "2022-32-24-14",
+	},
+
+	{
+		count: 2,
+		id: "1191634",
+		name: "Karmstol Öland",
+		OrderNumber: "2022-32-24-14",
+	},
+	,
+	{
+		count: 1,
+		id: "1191334",
+		name: "Matbord / Konferenssbord runt",
+		OrderNumber: "2022-32-24-14",
 	},
 ];
 
